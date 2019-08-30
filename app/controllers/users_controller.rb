25d4ac821613fd756bcb25f1e72d5d7529cfb367
Renkao102 <chono0316@gmail.com>
2019-08-30 15:16:42 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,7 @@
-class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
@@ -21,6 +12,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :group, :password, :password_confirmation)
     end
-end
+  　end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :group,  presence: true, length: { maximum: 50 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
